0a925d5aeb688a4aa7befbd1ad15c1950c9fddf9
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2023-06-08 18:57:44 +0200

    Finish registration modal component (UI only)
    This commit signifies the completion of the registration modal component without the actual functionality. The UI elements, layout, and design of the registration modal have been fully implemented.
    
    Although the component lacks the backend integration and form submission logic, it provides a solid foundation for incorporating the necessary functionality in future commits. The registration modal's user interface is now ready for further development and integration with the backend system.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import ClientOnly from "./components/ClientOnly";
 import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./components/providers/ToasterProvider";
 
 export const metadata = {
   title: 'Airbnb',
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
+          <ToasterProvider />
           <RegisterModal />
           <Navbar />
         </ClientOnly>
