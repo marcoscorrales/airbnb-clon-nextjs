4ffb7e6a9868eeb973176e7892b8cd019eb5399f
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2023-06-10 19:58:17 +0200

    Complete login functionality
    This commit marks the completion of the login functionality, allowing users to authenticate and access their accounts securely. The login form, validation, and integration with the authentication provider (e.g., NextAuth) are implemented.
    
    Users can now enter their credentials, submit the login form, and receive authentication feedback, such as success or error messages. The login functionality enhances the user experience by providing a secure and personalized environment within the application.
    
    This commit also includes any necessary updates to the UI, state management, and backend integration to support the login process.

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,10 +1,18 @@
 'use client';
+import { SafeUser } from "@/app/types";
 import Container from '../Container'
 import Logo from './Logo'
 import Search from './Search'
 import UserMenu from './UserMenu'
 
-const Navbar = () => {
+interface NavbarProps {
+  currentUser?: SafeUser | null;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ 
+  currentUser 
+}) => {
+  console.log({currentUser});
   return (
     <div className='fixed w-full bg-white z-10 shadow-sm'>
         <div className='py-4 border-b-[1px]'>
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import ClientOnly from "./components/ClientOnly";
 import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./components/providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
 
 export const metadata = {
   title: 'Airbnb',
@@ -12,18 +14,20 @@ export const metadata = {
 
 const font = Nunito({ subsets: ["latin"] });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const currentUser = await getCurrentUser();
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
         </body>
