9273c6f6af290c7cacc3616e04812428d25a45f8
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2023-06-10 18:25:34 +0200

    Complete registration modal functionality and Prisma setup
    This commit marks the completion of the registration modal functionality, including form validation, data submission, and integration with the Prisma ORM. Users can now successfully register and create accounts, with their information securely stored in the database.
    
    Additionally, the Prisma setup is finalized, ensuring seamless communication between the application and the database. Prisma's models, migrations, and query capabilities are fully utilized to manage and retrieve data efficiently.
    
    These updates provide a robust registration process and establish a solid foundation for data management within the application using Prisma's powerful features

--- pages/api/[...nextauth].ts
+++ pages/api/[...nextauth].ts
@@ -1,29 +1,28 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import NextAuth, { AuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google"; 
-import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcrypt"
+import NextAuth, { AuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 
-import bcrypt from "bcrypt";
-
-import prisma from "@/app/libs/prismadb";
+import prisma from "@/app/libs/prismadb"
 
 export const authOptions: AuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_CLIENT_ID as string,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET as string
-        }),
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string
+    }),
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
         }),
         CredentialsProvider({
-            name: 'Credentials',
+            name: 'credentials',
             credentials: {
                email: { label: 'email', type: 'text'},
-                password: { label: 'password', type: 'password'},
+               password: { label: 'password', type: 'password'},
             },
             async authorize(credentials) {
                 if(!credentials?.email || !credentials?.password) {
@@ -36,7 +35,7 @@ export const authOptions: AuthOptions = {
                     }
                 });
 
-                if(!user || user?.hashedPassword) {
+                if(!user || !user?.hashedPassword) {
                     throw new Error('Invalid credentials');
                 }
 
