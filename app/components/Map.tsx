85b21be92809107a9f711c52d29e4b1da12efa96
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2023-06-24 19:49:01 +0200

    Image upload with Cloudinary CDN

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -31,7 +31,7 @@ const Map: React.FC<MapProps> = ({
         center={center as L.LatLngExpression || [51, -0.09]}
         zoom={center ? 4 : 2}
         scrollWheelZoom={false}
-        className='h-[35hv] rounded-lg' 
+        className='h-[35vh] rounded-lg' 
     >
         <TileLayer
           url={url}
