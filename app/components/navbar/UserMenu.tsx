9e8060df4f0132b61733424e740ca32802ec00ee
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2023-07-12 20:13:56 +0200

    Properties screen

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -70,7 +70,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                 />
                 <MenuItem 
                   label="My properties" 
-                  onClick={() => {}}
+                  onClick={() => router.push('/properties')}
                 />
                 <MenuItem 
                   label="Airbnb your home" 
